45c86f94ae8b414c8f65e2e062df0e12658451d0
Nicolas Grenié <nicolas.grenie@gmail.com>
2014-12-03 01:01:27 +0100

    list it as currentevent an hour prior the event

--- client/helpers/commons.js
+++ client/helpers/commons.js
@@ -1,11 +1,20 @@
 // Event that are currently running
 // startDate <= currentDate <= endDate
+// one hour before
 Template.registerHelper('openEventOptions',function(){
-    var events = Events.find({$and:[{startDate:{$lte:new Date()}},{endDate:{$gte:new Date()}}]},{fields:{slug:1,name:1}}).fetch();
+    var events = Events.find({$and:[{startDate:{$lte:new Date().substractHours(1)}},{endDate:{$gte:new Date()}}]},{fields:{slug:1,name:1}}).fetch();
     var results = [];
     _.each(events, function(e) {
         results.push({label:e.name,value:e.slug})
     });
-    console.log(results)
     return results;
 })
+
+Date.prototype.substractHours= function(h){
+    this.setHours(this.getHours()-h);
+    return this;
+}
+Date.prototype.addHours= function(h){
+    this.setHours(this.getHours()+h);
+    return this;
+}
