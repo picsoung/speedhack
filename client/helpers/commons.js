36e3835c2a7b48eb314c44f7b214c8d4f9034ab6
Nicolas Grenié <nicolas.grenie@gmail.com>
2014-12-03 01:01:27 +0100

    list it as currentevent an hour prior the event

--- client/helpers/commons.js
+++ client/helpers/commons.js
@@ -1,11 +1,20 @@
 // Event that are currently running
 // startDate <= currentDate <= endDate
+// one hour before
 Template.registerHelper('openEventOptions',function()***REMOVED***
-    var events = Events.find(***REMOVED***$and:[***REMOVED***startDate:***REMOVED***$lte:new Date()***REMOVED******REMOVED***,***REMOVED***endDate:***REMOVED***$gte:new Date()***REMOVED******REMOVED***]***REMOVED***,***REMOVED***fields:***REMOVED***slug:1,name:1***REMOVED******REMOVED***).fetch();
+    var events = Events.find(***REMOVED***$and:[***REMOVED***startDate:***REMOVED***$lte:new Date().substractHours(1)***REMOVED******REMOVED***,***REMOVED***endDate:***REMOVED***$gte:new Date()***REMOVED******REMOVED***]***REMOVED***,***REMOVED***fields:***REMOVED***slug:1,name:1***REMOVED******REMOVED***).fetch();
     var results = [];
     _.each(events, function(e) ***REMOVED***
         results.push(***REMOVED***label:e.name,value:e.slug***REMOVED***)
 ***REMOVED***);
-    console.log(results)
     return results;
 ***REMOVED***)
+
+Date.prototype.substractHours= function(h)***REMOVED***
+    this.setHours(this.getHours()-h);
+    return this;
+***REMOVED***
+Date.prototype.addHours= function(h)***REMOVED***
+    this.setHours(this.getHours()+h);
+    return this;
+***REMOVED***
