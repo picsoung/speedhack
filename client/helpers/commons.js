300ec872c8ae907991107c589c4a1341580a8f33
Nicolas Grenié <nicolas.grenie@gmail.com>
2014-12-03 13:43:53 +0100

    rollback

--- client/helpers/commons.js
+++ client/helpers/commons.js
@@ -2,7 +2,7 @@
 // startDate <= currentDate <= endDate
 // one hour before
 Template.registerHelper('openEventOptions',function(){
-    var events = Events.find({$and:[{startDate:{$lte:new Date().substractHours(1)}},{endDate:{$gte:new Date()}}]},{fields:{slug:1,name:1}}).fetch();
+    var events = Events.find({$and:[{startDate:{$lte:new Date()}},{endDate:{$gte:new Date()}}]},{fields:{slug:1,name:1}}).fetch();
     var results = [];
     _.each(events, function(e) {
         results.push({label:e.name,value:e.slug})
