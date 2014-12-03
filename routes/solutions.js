f5b2067968d9e8cba9f52bedb5cfb021495690ed
Nicolas Grenié <nicolas.grenie@gmail.com>
2014-12-03 13:48:49 +0100

    rid of admin

--- routes/solutions.js
+++ routes/solutions.js
@@ -6,9 +6,9 @@ Router.route('/solutions/new',***REMOVED***
 ***REMOVED***
     onBeforeAction:function()***REMOVED***
         var username = Meteor.user().username
-        if(Roles.userIsInRole(Meteor.user(), [this.params.slug+"_judge","admin"]))***REMOVED***
-            this.next()
-***REMOVED***
+        // if(Roles.userIsInRole(Meteor.user(), [this.params.slug+"_judge","admin"]))***REMOVED***
+        //     this.next()
+        // ***REMOVED***
         var team = Teams.findOne(***REMOVED***$or:[***REMOVED***owner:username***REMOVED***,***REMOVED***teammate_1:username***REMOVED***,***REMOVED***teammate_2:username***REMOVED***]***REMOVED***)
         if(!team)***REMOVED***
             Notifications.error("Need a team","Create a team first");
