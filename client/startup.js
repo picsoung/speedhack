763754990afe86c3023fb2e42f2ec5d91bbe4e2a
Nicolas Grenié <nicolas.grenie@gmail.com>
2015-01-28 15:19:30 +0100

    new options on login

--- client/startup.js
+++ client/startup.js
@@ -5,7 +5,7 @@ if(Meteor.isClient){
         });
 
         Accounts.ui.config({
-           passwordSignupFields: 'USERNAME_ONLY'
+           passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
         });
     });
 }
