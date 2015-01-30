6545ab0ceab754518c5bde8203e8aa533e67b05a
Nicolas Grenié <nicolas.grenie@gmail.com>
2015-01-30 14:45:28 +0100

    add details to sponsor page

--- client/views/faq.js
+++ client/views/faq.js
@@ -2,9 +2,10 @@ Template.faqDetails.helpers(***REMOVED***
     questions: function()***REMOVED***
         var questions = [
     ***REMOVED***title:"Why should I attend SpeedHack?", answer:"SpeedHack is not your normal hackathon. Come experience a new format, with less time, more competition and more fun. Really, is there a better way to gain prizes in only 3 hours just coding?"***REMOVED***,
-    ***REMOVED***title:"What should I bring to SpeedHack?", answer:"SpeedHack is a coding contest. You should bring all the materials you need to code in code condition. Typical shortlist contains: laptop, power cord, headphones."***REMOVED***,
-***REMOVED***title:"When the challenges will be unveiled?",answer:"Challenges are usually revealed 5 to 10 minutes after the beginning of the event. The link to challenges is posted on the site and displayed in the room."***REMOVED***,
-***REMOVED***title:"How big could my team be?",answer:"SpeedHack is different than typical hackathons. To give a fair advantage to each team, we limit the number of team members to 2. You can be by yourself in a team, or be two."***REMOVED***,
+***REMOVED***title:"How much does it cost to participate?", answer:"It’s free for hackers, but make sure you get a ticket. Find the registering link on each individual event page."***REMOVED***,
+    ***REMOVED***title:"What should I bring to SpeedHack?", answer:"The Speedhack is a coding contest. You should bring everything you need to code in good condition. Typical shortlist contains: laptop, power cord, headphones. We also suggest to check the extra points list before heading to the Speedhack, you may want to bring a costume, a musical instrument or some other non coding related items. Snacks and drinks will be provided."***REMOVED***,
+***REMOVED***title:"When will the challenges be unveiled?",answer:"Challenges are usually revealed 5 to 10 minutes after the beginning of the event. The link to challenges is posted on the site and displayed in the room."***REMOVED***,
+***REMOVED***title:"How many people in a team?",answer:"SpeedHack is different than typical hackathons. To give a fair advantage to each team, we limit the number of team members to 2. You can participate on your own or with a another person. If you don't have a team prior to the event, you can form one on the day."***REMOVED***,
         ]
         return questions
 ***REMOVED***
